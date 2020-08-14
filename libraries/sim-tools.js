e55c40b797a30b0e86534983dc24fcb6613d4040
Akshay Shankar <sakshays.2000@gmail.com>
2020-08-14 15:32:10 +0530

    Added new simulation codes.

--- libraries/sim-tools.js
+++ libraries/sim-tools.js
@@ -5,30 +5,32 @@
  * @param  Number xb   [x coordinate of 1st point]
  * @param  Number yb   [y coordinate of 2nd point]
  * @param  Number rlen [rest length of spring]
+ * @param  Canvas canv [canvas instance to plot on]
  * @return
  */
 
-function drawspring(xa, ya, xb, yb, rlen) {
-  let n = parseInt(10 * rlen / 100)
+function drawSpring(xa, ya, xb, yb, rlen, canv) {
+  let n = parseInt(20 * rlen / 100)
   let step = ((xa - xb) ** 2 + (ya - yb) ** 2) ** 0.5 / n; //steps between vertices
   let ang = atan((yb - ya) / (xb - xa)); //angle between two points
   step = (xa > xb) ? (-step) : step; //step is increased from a to b
+  let flag = (xa > xb) ? -1 : 1;
 
-  noFill();
+  canv.noFill();
 
-  push();
-  translate(xa, ya);
-  rotate(ang, createVector(0, 0, 1));
+  canv.push();
+  canv.translate(xa, ya);
+  canv.rotate(ang, createVector(0, 0, 1));
 
-  beginShape();
-  vertex(0, 0);
-  vertex(step / 2, 0)
+  canv.beginShape();
+  canv.vertex(0, 0);
+  canv.vertex(step / 2, 0)
   for (let i = 1; i < n; i++) {
-    vertex(step * i, 5 * (-1) ** i)
+    canv.vertex(step * i, flag * 5 * (-1) ** i)
   }
-  vertex((n - 0.5) * step, 0);
-  vertex(n * step, 0);
-  endShape();
+  canv.vertex((n - 0.5) * step, 0);
+  canv.vertex(n * step, 0);
+  canv.endShape();
 
-  pop();
+  canv.pop();
 }
